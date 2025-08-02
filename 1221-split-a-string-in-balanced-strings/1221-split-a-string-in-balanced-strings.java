63d8480401ea25c3c3f7cb643e1d27ed7315619e
shynee esther jones <shinyestherjones@gmail.com>
2025-08-02 11:21:39 +0530

    Time: 0 ms (100%), Space: 41.4 MB (71.13%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0, balance = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == 'R') {
+                balance++;
+            } else {
+                balance--;
+            }
+
+            if (balance == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
